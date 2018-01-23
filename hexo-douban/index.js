bbd84b5cc76a2c74aa4e000367aa7e3071688ed3
chocoluffy <luffy.yu@mail.utoronto.ca>
2018-01-23 11:37:33 -0500

    Update hexo-douban with old working version

--- hexo-douban/index.js
+++ hexo-douban/index.js
@@ -2,5 +2,5 @@
 'use strict';
 
 hexo.extend.generator.register('books', require('./lib/books-generator'));
-hexo.extend.generator.register('movies', require('./lib/movies-generator'));
-hexo.extend.generator.register('games', require('./lib/games-generator'));
+// hexo.extend.generator.register('movies', require('./lib/movies-generator'));
+// hexo.extend.generator.register('games', require('./lib/games-generator'));
--- hexo-douban/lib/books-generator.js
+++ hexo-douban/lib/books-generator.js
@@ -1,20 +1,37 @@
-'use strict';
+// var request = require('urllib-sync').request;
+// var request = require("request");
+var request = require('sync-request');
+var ejs = require("ejs");
 
-var request = require('urllib-sync').request;
-var path = require('path');
-var ejs = require('ejs');
-var renderStar = require('./util').renderStar;
+function renderStar(num) {
+  switch (num) {
+  case "1":
+    return "★☆☆☆☆ 很差";
+  case "2":
+    return "★★☆☆☆ 较差";
+  case "3":
+    return "★★★☆☆ 还行";
+  case "4":
+    return "★★★★☆ 推荐";
+  case "5":
+    return "★★★★★ 力荐";
+  default:
+    return "";
+  }
+}
 
-function callApi(user, start, timeout) {
+function callApi(user, start) {
   var wish = [];
   var reading = [];
   var read = [];
-  var res = request('https://api.douban.com/v2/book/user/' + user + '/collections?start=' + start + '&count=100', {
-    timeout: timeout,
-    dataType: 'json'
-  });
-  for (var i in res.data.collections) {
-    var book = res.data.collections[i];
+  url = "https://api.douban.com/v2/book/user/" + user + "/collections?start=" + start + "&count=100";
+  var res = JSON.parse(request('GET', url).getBody('utf8'));
+  // console.log(res);
+  // var res = request("https://api.douban.com/v2/book/user/" + user + "/collections?start=" + start + "&count=100", {
+  //   dataType: 'json'
+  // });
+  for (var i in res.collections) {
+    var book = res.collections[i]
     var item = {
       image: book.book.image,
       alt: book.book.alt,
@@ -26,25 +43,24 @@ function callApi(user, start, timeout) {
       updated: book.updated.substring(0, 10),
       rating: book.book.rating.average,
       recommend: (book.rating ? renderStar(book.rating.value) : ''),
-      comment: (book.comment ? book.comment : '')
-    };
-    if (book.status === 'wish') {
-      wish.push(item);
-    } else if (book.status === 'read') {
-      read.push(item);
-    } else if (book.status === 'reading') {
-      reading.push(item);
+      comment: (book.comment ? book.comment: '')
+    }
+    if (book.status == "wish") {
+      wish.push(item)
+    } else if (book.status == "read") {
+      read.push(item)
+    } else if (book.status == "reading") {
+      reading.push(item)
     }
   }
-
   return {
-    'wish': wish,
-    'reading': reading,
-    'read': read,
-    'start': res.data.start,
-    'count': res.data.count,
-    'total': res.data.total
-  };
+    "wish": wish,
+    "reading": reading,
+    "read": read,
+    "start": res.start,
+    "count": res.count,
+    "total": res.total
+  }
 }
 
 module.exports = function(locals) {
@@ -52,12 +68,6 @@ module.exports = function(locals) {
   if (!config.douban || !config.douban.book) {//当没有输入book信息时，不进行数据渲染。
     return;
   }
-
-  var timeout = 10000;
-  if (config.douban.timeout) {
-    timeout = config.douban.timeout;
-  }
-
   var wish = [];
   var reading = [];
   var read = [];
@@ -65,14 +75,15 @@ module.exports = function(locals) {
   var res;
   var start = 0;
   do {
-    res = callApi(config.douban.user, start, timeout);
+    res = callApi(config.douban.user, start);
     wish = wish.concat(res.wish);
     reading = reading.concat(res.reading);
     read = read.concat(res.read);
     start = res.start + res.count;
-  } while (start < res.total);
+  } while ( start < res . total );
 
-  var contents = ejs.renderFile(path.join(__dirname, '/templates/book.ejs'), {
+  var contents = ejs.renderFile(__dirname + '/templates/book.ejs', {
+    'title': config.douban.book.title,
     'quote': config.douban.book.quote,
     'reading': reading,
     'wish': wish,
