6430a04fe1ce7e2d85fc039a84041ec9c0703c6f
chocoluffy <yushunzhe951104@gmail.com>
2018-06-24 12:21:58 -0400

    Update /weibo

--- source/submit-to-baidu.sh
+++ source/submit-to-baidu.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "http://chocoluffy.com/tech/
+http://chocoluffy.com/life/
+http://chocoluffy.com/books/
+http://chocoluffy.com/weibo/
+http://chocoluffy.com/about/" >> baidu_urls.txt
 echo Please input your baidu token:
 read token
 curl -H 'Content-Type:text/plain' --data-binary @baidu_urls.txt "http://data.zz.baidu.com/urls?site=chocoluffy.com&token=$token"
