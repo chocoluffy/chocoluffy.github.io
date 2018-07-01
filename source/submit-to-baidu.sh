563fbfd96673d219fd33834bf26c4d97894b6322
chocoluffy <yushunzhe951104@gmail.com>
2018-07-01 21:42:53 +0800

    Update baidu submit script to create amp links txt

--- source/submit-to-baidu.sh
+++ source/submit-to-baidu.sh
@@ -5,7 +5,7 @@ echo Please input your baidu token:
 read token
 
 # copy baidu_urls.txt to baidu_amp_urls.txt and append '/amp' to all links.
-sed -i.bak '!/[^[:blank:]]/s/$/\/amp\//' baidu_urls.txt > baidu_amp_urls.txt
+awk '$0=$0"amp/"' baidu_urls.txt > baidu_amp_urls.txt
 
 # append more info link to baidu_urls.txt.
 echo "\nhttp://chocoluffy.com/tech/\n\
@@ -15,4 +15,4 @@ http://chocoluffy.com/weibo/\n\
 http://chocoluffy.com/about/" >> baidu_urls.txt
 
 curl -H 'Content-Type:text/plain' --data-binary @baidu_urls.txt "http://data.zz.baidu.com/urls?site=chocoluffy.com&token=$token"
-# curl -H 'Content-Type:text/plain' --data-binary @baidu_amp_urls.txt "http://data.zz.baidu.com/urls?site=chocoluffy.com&token=$token&type=amp"
+curl -H 'Content-Type:text/plain' --data-binary @baidu_amp_urls.txt "http://data.zz.baidu.com/urls?site=chocoluffy.com&token=$token&type=amp"
