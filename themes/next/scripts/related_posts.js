c35144b94ab3e0d020ba4d694e695850ba181e3b
chocoluffy <yushunzhe951104@gmail.com>
2018-12-15 17:20:21 -0500

    Try add post page view to related post widget.

--- themes/next/scripts/related_posts.js
+++ themes/next/scripts/related_posts.js
@@ -32,10 +32,10 @@ hexo.extend.helper.register('related_posts', function(currentPost, allPosts){
                     '">' + 
                     relatedPosts[i].title +
                     '</a>' + 
-                    '<span id="/' + relatedPosts[i].path +  '" class="leancloud_visitors" data-flag-title="' + relatedPosts[i].title + '">' + 
-                        '<span class="post-meta-item-text">&nbsp; , &nbsp;阅读次数: </span>' + 
-                        '<span class="leancloud-visitors-count"></span>'
-                    '</span>'
+                    // '<span id="/' + relatedPosts[i].path +  '" class="leancloud_visitors" data-flag-title="' + relatedPosts[i].title + '">' + 
+                    //     '<span class="post-meta-item-text">&nbsp; , &nbsp;阅读次数: </span>' + 
+                    //     '<span class="leancloud-visitors-count"></span>'
+                    // '</span>'
                     '</li>';
     };
     result += '</ul>';
